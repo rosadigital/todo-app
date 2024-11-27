eae662416b366100ff12ff5b3d68c4aa99360a73
Kazi Islam <kzislam@gmail.com>
2024-11-26 22:06:17 -0500

    updated vite config for cloud hosting

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 //api.js
 import axios from "axios";
-const API_URL = "http://localhost:3000/api"; //
+const API_URL = "https://todoapp-server-hr2a.onrender.com/api"; //
 const api = axios.create({
   baseURL: API_URL,
 });
